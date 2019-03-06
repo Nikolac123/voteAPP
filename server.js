774fdb34de1fabce635b292ed6605a03236023ed
Nikola Cakar <nikola.cakar10@gmail.com>
2019-03-06 20:15:49 +0100

    dodadovme citanje glasovi

--- server.js
+++ server.js
@@ -7,13 +7,16 @@ let server = express()
 server.use (express.json())
 server.use (express.static("./klient"))
 
-
 server.post("/vote", (req, res) => {
     let data = req.body
     let polBr = data.politicarId
    glasovi[polBr] = glasovi[polBr] + 1;
     console.log(glasovi)
     res.send("OK")
+})
+
+server.get("/votes", (req, res) => {
+res.send(glasovi)
 })
 
 server.listen(4000)
